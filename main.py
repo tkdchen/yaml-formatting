eb7505040022a1e918fcd06cfeb577369be00939
Chenxiong Qi <qcxhome@gmail.com>
2025-07-24 10:05:49 +0800

    Add list example in flow style

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import argparse
 from ruamel.yaml import YAML
 from subprocess import run
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--yq-in-advance", action="store_true", help="Run yq before loading test YAML file.")
+args = parser.parse_args()
+
 yaml_file = "./test.yaml"
 
-run(["yq", "-i", yaml_file], check=True)
-run(["cat", yaml_file])
-
+if args.yq_in_advance:
+    run(["yq", "-i", yaml_file], check=True)
+    run(["cat", yaml_file])
 
 yaml = YAML()
 yaml.width = 4096
